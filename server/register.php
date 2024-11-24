c88566c9fad4c80c2ff4c24d84e1a620eb589289
Jan Lancelot P. Mailig <132678636+JanLancelot@users.noreply.github.com>
2024-11-24 08:09:05 +0800

    Added genres and tmdb_id to api

--- server/register.php
+++ server/register.php
@@ -1,5 +1,4 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
@@ -16,15 +15,20 @@ $data = json_decode(file_get_contents("php://input"));
 
 $username = $data->username;
 $password = password_hash($data->password, PASSWORD_DEFAULT);
+$firstName = $data->firstName;
+$lastName = $data->lastName;
+$middleName = $data->middleName;
+$contactNo = $data->contactNo;
+$role = $data->role;
 
-$sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+$sql = "INSERT INTO users (username, password, first_name, last_name, middle_name, contact_no, role) VALUES (?, ?, ?, ?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", $username, $password);
+$stmt->bind_param("sssssss", $username, $password, $firstName, $lastName, $middleName, $contactNo, $role);
 
 if ($stmt->execute()) {
     echo json_encode(array("message" => "User registered successfully"));
 } else {
-    echo json_encode(array("message" => "Unable to register user"));
+    echo json_encode(array("message" => "Unable to register user", "error" => $conn->error));
 }
 
 $stmt->close();
